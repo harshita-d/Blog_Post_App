08bc9f354ebdb5fb6061f5eb1175c73697007bc3
harshita <hrs.dadhich@gmail.com>
2024-04-21 15:04:48 +0530

    url changes

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blogposts/", views.BlogPostCreateList.as_view(), name="blogpost_view_create")
+    path("", views.BlogPostCreateList.as_view(), name="blogpost_view_create")
 ]
 
